e66c431ecd58823549d30969a52bd3d3c1bc2f69
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2022-11-21 14:45:48 +0100

    Use NOW in entrypoint as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 echo $POSTGRES_HOSTNAME:5432:$POSTGRES_USERNAME:$POSTGRES_USERNAME:$POSTGRES_PASSWORD > ~/.pgpass
 chmod 600 ~/.pgpass
-pg_dump -h $POSTGRES_HOSTNAME -U $POSTGRES_USERNAME --no-owner --no-acl $POSTGRES_DATABASE -Fc > /backup/dump.latest-`date "+%Y%m%d"`
-node src/upload.js --filename=/backup/dump.latest-%TODAY --folder-id=$GOOGLE_DRIVE_PARENT_FOLDERID
-rm /backup/dump.latest-`date "+%Y%m%d"`
-node src/ensure-max-number-files.js --maximum-number-files=2 --folder-id=$GOOGLE_DRIVE_PARENT_FOLDERID
+pg_dump -h $POSTGRES_HOSTNAME -U $POSTGRES_USERNAME --no-owner --no-acl $POSTGRES_DATABASE -Fc > /backup/dump.latest-`date "+%Y%m%d-%H%M"`
+node src/upload.js --filename=/backup/dump.latest-%NOW --folder-id=$GOOGLE_DRIVE_PARENT_FOLDERID
+rm /backup/dump.latest-`date "+%Y%m%d-%H%M"`
+node src/ensure-max-number-files.js --maximum-number-files=5 --folder-id=$GOOGLE_DRIVE_PARENT_FOLDERID
